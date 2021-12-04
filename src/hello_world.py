c514a7ef8123244f54819e099e39a85fbf7c32e3
Christian Schmidli <christian.schmidli@gmail.com>
2021-12-04 12:02:35 +0100

    init

---
+++ src/hello_world.py
@@ -0,0 +1,4 @@
+def hello_name(name:str):
+    msg = "hello " + name
+    print(msg)
+    return msg
---
+++ test/test_all.py
@@ -0,0 +1,5 @@
+from src.hello_world import hello_name
+
+
+def test_all():
+    hello_name("bla")
